1afe08d5ebb04a5cc43a008339731d6fd0942ada
vuonghoainam <vuonghoainam@gmail.com>
2018-03-16 12:40:01 +0700

    Add variables.tf to the source

---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  default = "us-west-2"
+}
+variable "aws_key_name" {
+    default = "grap"
+}
+variable "aws_key_path" {
+    default = "./aws_credentials"
+}
+variable "aws_instance_type" {
+    default = "t2.nano"
+}
+variable "aws_instance_user" {
+    default = "ubuntu"
+}
+variable "aws_amis" {
+    default = {
+        us-west-2 = "ami-e1906781"
+    }
+}
+variable "aws_availability_zones" {
+    default = {
+        "0" = "us-west-2a",
+        "1" = "us-west-2b",
+        "3" = "us-west-2c"
+    }
+}
+
+variable "aws_security_group" {
+    default = {
+        sg_count                = 1
+
+        sg_0_name               = "http"
+        sg_0_ingress_from_port  = 80
+        sg_0_ingress_to_port    = 80
+        sg_0_protocol           = "http"
+    }
+}
